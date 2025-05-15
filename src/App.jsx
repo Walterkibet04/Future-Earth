217d3547353353588e8ab6c7cd2aeb543de71ba1
walter <kibetwalter726@gmail.com>
2025-05-15 18:35:52 +0300

    update on app.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,7 @@ import { BrowserRouter, Route , Routes} from 'react-router-dom'
 import Home from './pages/Home'
 import SustainableLiving from './pages/SustainableLiving'
 import Contact from './pages/Contact'
-import OurStory from './pages/Ourstory'
+import OurStory from './pages/OurStory'
 import MenuPage from './components/MenuPage'
 function App() {
 
